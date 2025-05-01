69c84a64e9da74fdee8e49f26ac0f2065be2d2e5
AJ Dumanhug <9420289+ajdumanhug@users.noreply.github.com>
2025-05-02 06:04:08 +0800

    Fix stuff

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,19 @@
 import createMDX from '@next/mdx';
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const baseConfig = {
   reactStrictMode: true,
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
   devIndicators: false,
   output: 'export',
   images: {
     unoptimized: true,
-  }, 
+  },
 };
 
 const withMDX = createMDX({
   extension: /\.mdx?$/,
 });
 
-export default withMDX(nextConfig);
+const nextConfig = withMDX(baseConfig);
+export default nextConfig;
